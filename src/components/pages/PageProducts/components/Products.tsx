704ac6a73ceb5d769adf7bf6f3226711594aaba4
Serhii <ramram614@gmail.com>
2024-03-26 13:08:07 +0200

    FE task-3

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -10,7 +10,6 @@ import { useAvailableProducts } from "~/queries/products";
 
 export default function Products() {
   const { data = [], isLoading } = useAvailableProducts();
-
   if (isLoading) {
     return <Typography>Loading...</Typography>;
   }
@@ -19,15 +18,14 @@ export default function Products() {
     <Grid container spacing={4}>
       {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
       {data.map(({ count, ...product }, index) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4}>
+         <Grid item key={product.id} xs={12} sm={6} md={4}>
           <Card
             sx={{ height: "100%", display: "flex", flexDirection: "column" }}
           >
             <CardMedia
               sx={{ pt: "56.25%" }}
               image={`https://source.unsplash.com/random?sig=${index}`}
-              title="Image title"
-            />
+              title="Image title" />
             <CardContent sx={{ flexGrow: 1 }}>
               <Typography gutterBottom variant="h5" component="h2">
                 {product.title}
